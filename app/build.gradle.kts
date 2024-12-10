a63d704d81c04baf7a3a6f8b4e79b3bedc5fc68a
chaser <fedenev.ilya.dm@gmail.com>
2024-12-10 16:35:30 +0700

    Add upgrades, auto coins, coins for click

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "dev.chsr.justclick"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.chsr.justclick"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/dev/chsr/justclick/MainActivity.kt
+++ app/src/main/java/dev/chsr/justclick/MainActivity.kt
@@ -1,47 +1,157 @@
 package dev.chsr.justclick
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.chsr.justclick.ui.theme.JustClickTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val buttonColors = ButtonColors(
+            containerColor = Color.Black,
+            contentColor = Color.White,
+            disabledContentColor = Color.LightGray,
+            disabledContainerColor = Color.DarkGray
+        )
+        val scope = CoroutineScope(Dispatchers.Main)
+        val df = DecimalFormat("#.##")
+
         setContent {
             JustClickTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    val coins = remember { mutableFloatStateOf(0f) }
+                    val coinsForClick = remember { mutableFloatStateOf(1f) }
+                    val autoCoins = remember { mutableFloatStateOf(0.1f) }
+                    LaunchedEffect(scope) {
+                        scope.launch {
+                            while (isActive) {
+                                coins.floatValue += autoCoins.floatValue
+                                delay(1000)
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier.align(Alignment.Center)
+                    ) {
+                        Text(
+                            text = df.format(coins.floatValue),
+                            fontSize = 48.sp,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(10.dp)
+                        )
+                        Button(
+                            onClick = {
+                                coins.floatValue += coinsForClick.floatValue
+                            },
+                            colors = buttonColors,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(10.dp)
+                        ) {
+                            Text(
+                                text = "+${df.format(coinsForClick.floatValue)}",
+                                textAlign = TextAlign.Center,
+                                fontSize = 18.sp,
+                                modifier = Modifier.padding(10.dp)
+                            )
+                        }
+                        Text(
+                            text = "Auto: ${df.format(autoCoins.floatValue)}/s",
+                            fontSize = 16.sp,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(10.dp)
+                        )
+                    }
+                    Column(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 30.dp)
+                    ) {
+                        Button(
+                            onClick = {
+                                if (coins.floatValue >= coinsForClick.floatValue * 10f) {
+                                    coins.floatValue -= coinsForClick.floatValue * 10f
+                                    coinsForClick.floatValue += coinsForClick.floatValue / 10f
+                                }
+                            },
+                            colors = buttonColors,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(bottom=10.dp)
+                        ) {
+                            Text(
+                                text = df.format(coinsForClick.floatValue * 10f),
+                                textAlign = TextAlign.Left,
+                                modifier = Modifier.padding(end = 30.dp)
+                            )
+                            Text(
+                                text = "Upgrade coins for click",
+                                textAlign = TextAlign.Center
+                            )
+                            Text(
+                                text = "+${df.format(coinsForClick.floatValue / 10f)}",
+                                textAlign = TextAlign.Right,
+                                modifier = Modifier.padding(start = 30.dp)
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                if (coins.floatValue >= autoCoins.floatValue * 10f) {
+                                    coins.floatValue -= autoCoins.floatValue * 10f
+                                    autoCoins.floatValue += autoCoins.floatValue / 10f
+                                }
+                            },
+                            colors = buttonColors,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        ) {
+                            Text(
+                                text = df.format(autoCoins.floatValue * 10f),
+                                textAlign = TextAlign.Left,
+                                modifier = Modifier.padding(end = 30.dp)
+                            )
+                            Text(
+                                text = "Upgrade auto coins",
+                                textAlign = TextAlign.Center
+                            )
+                            Text(
+                                text = "+${df.format(autoCoins.floatValue / 10f)}",
+                                textAlign = TextAlign.Right,
+                                modifier = Modifier.padding(start = 30.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JustClickTheme {
-        Greeting("Android")
-    }
-}
